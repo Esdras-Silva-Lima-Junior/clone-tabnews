2edd8f4419fe7e335886830751c859fdc6537777
Esdras Silva de Lima Junior <98972552+Esdras-Silva-Lima-Junior@users.noreply.github.com>
2024-09-30 19:12:44 +0000

    run dev

---
+++ .next/server/pages/index.js
@@ -0,0 +1,46 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/index";
+exports.ids = ["pages/index"];
+exports.modules = {
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nfunction Home() {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h1\", {\n        children: \"Hello world!!!\"\n    }, void 0, false, {\n        fileName: \"/workspaces/clone-tabnews/pages/index.js\",\n        lineNumber: 2,\n        columnNumber: 10\n    }, this);\n}\n// Para o Next.js saber qual função deve ser exportada e renderizada, pra isso precisamos informar com o comando export qual a função que queremos usar na exibição, para podermos definir a exportação, precisamos usar o comando \"export default *NOME DA FUNÇÃO*;\".\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Home);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9pbmRleC5qcy5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7O0FBQUEsU0FBU0EsT0FBTztJQUNkLHFCQUFPLDhEQUFDQztrQkFBRzs7Ozs7O0FBQ2I7QUFFQSxzUUFBc1E7QUFFdFEsaUVBQWVELElBQUlBLEVBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9jbG9uZS10YWJuZXdzLy4vcGFnZXMvaW5kZXguanM/YmVlNyJdLCJzb3VyY2VzQ29udGVudCI6WyJmdW5jdGlvbiBIb21lKCkge1xuICByZXR1cm4gPGgxPkhlbGxvIHdvcmxkISEhPC9oMT47XG59XG5cbi8vIFBhcmEgbyBOZXh0LmpzIHNhYmVyIHF1YWwgZnVuw6fDo28gZGV2ZSBzZXIgZXhwb3J0YWRhIGUgcmVuZGVyaXphZGEsIHByYSBpc3NvIHByZWNpc2Ftb3MgaW5mb3JtYXIgY29tIG8gY29tYW5kbyBleHBvcnQgcXVhbCBhIGZ1bsOnw6NvIHF1ZSBxdWVyZW1vcyB1c2FyIG5hIGV4aWJpw6fDo28sIHBhcmEgcG9kZXJtb3MgZGVmaW5pciBhIGV4cG9ydGHDp8OjbywgcHJlY2lzYW1vcyB1c2FyIG8gY29tYW5kbyBcImV4cG9ydCBkZWZhdWx0ICpOT01FIERBIEZVTsOHw4NPKjtcIi5cblxuZXhwb3J0IGRlZmF1bHQgSG9tZTtcbiJdLCJuYW1lcyI6WyJIb21lIiwiaDEiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./pages/index.js\n");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/index.js"));
+module.exports = __webpack_exports__;
+
+})();
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function Home() {
+  return <h1>Hello world!!!</h1>;
+}
+
+// Para o Next.js saber qual função deve ser exportada e renderizada, pra isso precisamos informar com o comando export qual a função que queremos usar na exibição, para podermos definir a exportação, precisamos usar o comando "export default *NOME DA FUNÇÃO*;".
+
+export default Home;

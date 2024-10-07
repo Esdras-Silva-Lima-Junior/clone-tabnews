8c9f688f30e7d005c9a031b5478676edab3bd715
Esdras Silva de Lima Junior <98972552+Esdras-Silva-Lima-Junior@users.noreply.github.com>
2024-10-07 22:34:51 +0000

    changing the message

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Hello world!!! I'm Esdras Silva and I'm testing a course.</h1>;
+  return <h1>Hi, I'm Esdras Silva de Lima Junior and I am a student of programming, I love all linked with it and I love learn everything about it.</h1>;
 }
 
 export default Home;
